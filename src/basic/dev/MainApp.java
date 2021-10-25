28d62cec26459e604fb0a14a8829a6fb36fe6151
biaphan118 <biaphan2002@gmail.com>
2021-10-25 00:24:08 -0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,33 @@
+package basic.dev;
+/**
+ * Ghi chú(comment nhiều dòng)
+ * @author biaprfo
+ * 1. Tên lớp trùng tênfile
+ * 2. Tên lớp chữ cái đầu viết hoa. Nếu từ ghép thì những chữ cái dầu viết hoa
+ * 4. Tên lớp đặt phải có ý nghĩa. Ví dụ: class Peson: lớp mô tả con người
+ * -Interface: Phải có tiền tố I. Ví dụ: IListener
+ * -Abstract: Phải có tiền tố Abstract. Ví dụ: AbstractShape
+ */
+
+public class MainApp {
+	//Comment 1 dòng cách 1. Phím nóng Ctr+ /
+	/* Comment 1 dòng cách 2*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello World");
+         /**
+          * 1. Không chứa ký tự đặc biệt !@#$%^&*()-+=
+          * 2. Không chứa khoảng trắng
+          * 3. Viết thường
+          * 4. Không được bắt đầu bằng số
+          */
+         int number;
+         String nameSchool;
+	}
+	/*
+	 * tên phương thức tuân theo quy tắc camelCase
+	 */
+public void show () {
+}
+}
